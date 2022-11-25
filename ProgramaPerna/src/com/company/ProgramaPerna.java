81f32508b07698af8ed02124b4544340cec0ed7a
ana <aninha_rsantos@yahoo.com.br>
2022-11-25 18:43:57 -0300

    subida programa perna e voto obrigatório

---
+++ ProgramaPerna/src/com/company/ProgramaPerna.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ProgramaPerna {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Quantas pernas? ");
+        int perna = teclado.nextInt();
+        String tipo;
+        System.out.print("Isso é um(a) ");
+        switch (perna) {
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bipede";
+                break;
+            case 3:
+                tipo = "Tripé";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";
+                break;
+        }
+        System.out.println(tipo);
+    }
+}
---
+++ VotoObrigatorio/src/com/company/VotoObrigatorio.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class VotoObrigatorio {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String ano = "yyyy";
+        int nascimento;
+
+        SimpleDateFormat formatacaoAno = new SimpleDateFormat(ano);
+        String dataDoSistema = formatacaoAno.format(new Date());
+
+        System.out.print("Qual o ano de nascimento: ");
+        nascimento = teclado.nextInt();
+
+        int idade = Integer.parseInt(dataDoSistema) - nascimento;
+        System.out.println("Sua idade: " + idade);
+
+        if (idade < 16) {
+            System.out.print("Não vota!");
+        } else {
+            if ((idade >= 16 && idade < 18) || (idade > 70)) {
+                System.out.print("Opcional");
+            } else {
+                System.out.print("Voto obrigatório!");
+            }
+        }
+    }
+}
