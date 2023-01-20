ecf11e555b31d486d65a4e6b9f5b6dea6fc99f8d
ana <aninha_rsantos@yahoo.com.br>
2023-01-20 16:25:45 -0300

    inserindo numeros no vetor

--- VetorOpcoes/src/com/company/VetorOpcoes.java
+++ VetorOpcoes/src/com/company/VetorOpcoes.java
@@ -7,22 +7,17 @@ public class VetorOpcoes {
         Scanner teclado = new Scanner(System.in);
 
         int vetor[] = new int [5];
-        int numerosDigitados[] = new int[0];
+        Integer numerosDigitados[] = new Integer[vetor.length];
 
         for (int contador = 0; contador <5; contador++) {
             System.out.print("Digite 5 números entre 0 e 5: ");
             numerosDigitados[contador] = teclado.nextInt();
-        }
-
-
-        if (numerosDigitados < 0) {
-            System.out.println("Número inválido, por favor, digite um número entre 0 e 5");
-        } else {
-
-            vetor[0] = numerosDigitados;
-            System.out.println(vetor[0]);
-
-
+            if (numerosDigitados[contador] < 0 || numerosDigitados[contador] > 5) {
+                System.out.println("Número inválido, por favor, digite um número entre 0 e 5.");
+            } else {
+                vetor[contador] = numerosDigitados[contador];
+                System.out.println("[" + numerosDigitados[contador] + "]");
+            }
         }
     }
 }
